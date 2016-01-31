7faf71614bd00fbdfb578c792cbe5a27e4c1ca0e
Christian Fritz <christian.fritz@gmail.com>
2016-01-30 20:26:59 -0800

    Polish
    - also simplified phantoms cript now that we clean up
    after blaze

--- phantom_get_html.js
+++ phantom_get_html.js
@@ -1,17 +1,7 @@
-var webpage = require('webpage');
-
-page = webpage.create();
+var page = require('webpage').create();
 page.open('blaze.html', function(status) {
     window.setTimeout(function() {
-        var content = page.content;
-        var bodyStart = content.indexOf("<body>");
-        console.log(
-          // don't replace myscript with script in head, because there
-          // it appears inside another script tag
-          content.slice(0, bodyStart),
-          // but do so in the body
-          content.slice(bodyStart).replace(/myscript/g, "script")
-        );
+        console.log(page.content.replace(/myscript/g, "script"));
         phantom.exit();
       }, 1000);
   });
